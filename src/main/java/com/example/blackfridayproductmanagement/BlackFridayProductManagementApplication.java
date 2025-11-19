ece21d8d899ed07b1b11209b226bcd4ba9f8ed26
Lewis CRANE <103712344+LewisProg971@users.noreply.github.com>
2025-11-19 10:03:42 +0100

    first

---
+++ src/main/java/com/example/blackfridayproductmanagement/BlackFridayProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackfridayproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackFridayProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackFridayProductManagementApplication.class, args);
+    }
+
+}
